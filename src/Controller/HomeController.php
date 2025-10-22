143b2975ffb8c42b31b8b2a36d819b2d9e8689a2
Jhonatan Fernandez <prueba@MacBook-Pro-de-prueba.local>
2025-10-22 05:49:04 -0400

    Se creo el controlador Home. Y se puso estilos y se puso en el base.html ese estilo, tambien en el index del home, se puso el enlace de logout

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+
+    #[Route(path: '/', name: 'app_root')]
+    public function root(): Response
+    {
+        return $this->redirectToRoute('app_login');
+    }
+
+
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
